216309701f6dda6e1242dc4a4b88a415ba68395f
Addison <addison@framer.com>
2019-07-26 12:27:13 +0200

    Update test suites

--- gatsby-config.js
+++ gatsby-config.js
@@ -169,7 +169,7 @@ module.exports = {
         background_color: "#f7f0eb",
         theme_color: "#a2466c",
         display: "minimal-ui",
-        icon: "favicon/favicon.png"
+        icon: "static/favicon/favicon.png"
       }
     },
     "gatsby-plugin-sitemap",
